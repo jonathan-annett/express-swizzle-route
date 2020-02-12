76aa350b6a76db18783b1845c9db79a3b9c35da1
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-12 12:14:41 +1100

    added options.duplicatesHandler for testing

--- index.js
+++ index.js
@@ -12,34 +12,25 @@ function createSwizzledRoute(app,method,path,options) {
     
     // establish a (quasi?) async setup callback to call for the first time the 
     // handler is called by an external browser / remote client 
-    var doSetup = options.setup;
-
-    if (typeof doSetup !== 'function') {
-        doSetup = function(options,cb) {
-            // there is no setup code so just invoke callback immedately
-            cb();
-        };
-    }
     
-    
+    var doSetup = typeof options.setup === 'function' ? swizzleSetup : noSetup ;
+        
     // install the temporary "first time handler for the url path"
     app[method](path,function(req,res){
         
-        //backup the swizzling arguments...
+       
         if (options.permitUndo){
+            // if options.permitUndo is truthy, calls to createSwizzledRoute.undo(path) will
+            // restore the path to an unswizzled state, after calling options.teardown() for if it has been defined
+            
+            //backup the swizzling arguments...
             if (swizzled[path]) swizzled[path].splice(0,4);
             swizzled[path]=[app,method,path,options];
         }
         
         // remove the swizzled route
         removeRoute(app,path);
-        
-        // install a temporary stack to hold any requessts that come in while setup is happening
-        options.temp_stack=[];
-        app[method](path,function(req,res){
-            options.temp_stack.push({req:req,res:res});
-        });
-        
+
         // perform any setup required for the handler's first invocation
         doSetup (options,function(){
             // once we are fully setup, remove the temp handler install the defined handler
@@ -48,22 +39,56 @@ function createSwizzledRoute(app,method,path,options) {
             // handle the "first" request, which kicked off the setup/swizzle process
             options.handler(req,res);
             
-            // in most cases, temp_stack will have no entries.
-            // especially if there was no setup, of if setup was synchronous.
-            // if however additional requests came in while setup was happening, 
-            // they will be in temp_stack, so we handle them now.
-            if (options.temp_stack.length>0) {
-                options.temp_stack.forEach(function(backlog){
-                    options.handler(backlog.req,backlog.res);                
-                });
-                options.temp_stack.splice(0,options.temp_stack.length);
+            if (options.duplicatesHandler) {
+                options.handler = options.duplicatesHandler;
             }
-            delete options.temp_stack;
 
         });            
 
         
     });
+    
+    /*
+      swizzleSetup() is invoked to provide user defined setup for the first time the request is invoked
+      this could be doing anything from allocating memory, fetching data from another server or reading 
+      info in from a disk file or database.
+      it could also be setting up other routes that only make sense if this route exists.
+      
+      because it could be time expensive, and other requests may come in during the setup callback
+      pending requests are stacked inside options.temp_stack, which is processed once the first
+      request has been handled.
+    
+    */
+    function swizzleSetup(options,cb) {
+        // install a temporary stack to hold any requests that come in while setup is happening
+        options.temp_stack=[];
+        app[method](path,function(req,res){
+            // push this request onto the temp stack until after the setup has completed
+            options.temp_stack.push({req:req,res:res});
+        });
+        
+        options.setup(options,function(){
+            
+            // now setup is complete call cb to install the new handler, and invoke it for the first request
+            cb();
+            
+            // in most cases, temp_stack will have no entries.
+            // if additional requests came in while setup was happening, 
+            // they will be in temp_stack, so we handle them now.
+            while (options.temp_stack.length>0) {
+                var backlog = options.temp_stack.shift();
+                options.handler(backlog.req,backlog.res);                
+            }
+            delete options.temp_stack;
+
+        });
+    } 
+    
+    function noSetup(options,cb) {
+        // there is no setup code so just invoke callback immediately
+        // note: we don't install a temp_stack.
+        cb();
+    }
     
 }
 
@@ -75,8 +100,9 @@ createSwizzledRoute.undo = function undoSwizzledRoute(path) {
    };
    if (args) {
        delete swizzled[path];
-       if (args[3].teardown) {
-           args[3].teardown(args[3],continue_);
+       var options = args[3];
+       if (options.teardown) {
+           options.teardown(options,continue_);
        } else {
            continue_();
        }
